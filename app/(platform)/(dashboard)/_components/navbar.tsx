5f379e78806b27c2c56d06de896c04d0f5c5818e
Haidar <afiq.haidar12@gmail.com>
2024-03-04 01:54:04 +0700

    fix: object align

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,11 @@
-import Logo from "@/components/logo";
+import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 
 export const Navbar = () => {
   return (
-    <div className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+    <div className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
       {/*Mobile Sidebar*/}
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
@@ -26,7 +26,7 @@ export const Navbar = () => {
           <Plus className="h-4 w-4" />
         </Button>
       </div>
-      <div className=" ml-auto flex items-center  gap-x-2">
+      <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
           hidePersonal
           afterCreateOrganizationUrl="/organization/:id"
@@ -34,9 +34,11 @@ export const Navbar = () => {
           afterSelectOrganizationUrl="/organization/:id"
           appearance={{
             elements: {
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              },
             },
           }}
         />
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,26 +1,27 @@
-import Image from "next/image";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
+import Image from "next/image";
 import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
 
 const headingFont = localFont({
   src: "../public/fonts/font.woff2",
 });
 
-const Logo = () => {
+export const Logo = () => {
   return (
     <Link href="/">
-      <div
-        className="hover:opacity-75 transition
-      items-center gap-x-2 hidden md:flex"
-      >
-        <Image src="/logo.svg" alt="logo" height={30} width={30} />
-        <p className={cn("text-lg text-neutral-900", headingFont.className)}>
+      <div className="hover:opacity-75 transition items-center gap-x-1 hidden md:flex">
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p
+          className={cn(
+            "text-lg text-neutral-700 pt-0.5",
+            headingFont.className
+          )}
+        >
           Task
         </p>
       </div>
     </Link>
   );
 };
-
-export default Logo;
