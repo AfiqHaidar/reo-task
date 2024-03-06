7c82a1f533ae343d53e627162f7c526d0dba5a11
Haidar <afiq.haidar12@gmail.com>
2024-03-07 06:13:54 +0700

    init: server action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,21 @@
+import { create } from "@/actions/create-dashboard";
+import { Button } from "@/components/ui/button";
+
 const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+  return (
+    <div>
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Board Title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
